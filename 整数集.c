1d2ca6b01ec6f863dd045663c78550d5bafed5cd
Brooks Yu <especiallymaymars@gmail.com>
2026-06-15 16:02:14 +0800

    :new project

--- 整数集.c
+++ 整数集.c
@@ -5,9 +5,31 @@ int main()
     int a;
     scanf("%d", &a);
 
-    for (int i = a; i < a + 3; i++ {
-             for (j)
-         })
+    int count = 0;
 
-        return 0;
+    for (int i = a; i <= a + 3; i++)
+    {
+        for (int j = a; j <= a + 3; j++)
+        {
+            for (int k = a; k <= a + 3; k++)
+            {
+                if (i != k && i != j && j != k)
+                {
+                    count++;
+                    printf("%d%d%d", i, j, k);
+
+                    if (count % 6 == 0)
+                    {
+                        printf("\n");
+                    }
+                    else
+                    {
+                        printf(" ");
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
 }
