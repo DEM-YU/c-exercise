6c5fa833df5dad1267d7ba3a39f0751f82be75ec
Brooks Yu <especiallymaymars@gmail.com>
2026-06-16 00:36:12 +0800

    :more

---
+++ 水仙花数.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int start = 1;
+    for (int i = 1; i < n; i++)
+    {
+        start *= 10;
+    }
+    int end = start - 1;
+
+    for (int num = start; num < start; num++)
+    {
+    }
+
+    return 0;
+}
